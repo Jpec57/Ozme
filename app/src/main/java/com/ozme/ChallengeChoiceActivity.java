5f6a4ecad854b017e19943543a405a920a3b22f0
Jpec57 <jean.paul.bella@hotmail.fr>
2017-12-29 01:08:44 +0100

    Profile pic hard to get

---
+++ app/src/main/java/com/ozme/ChallengeChoiceActivity.java
@@ -0,0 +1,12 @@
+package com.ozme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChallengeChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
